a63d2ecd7c4ebf75d9b859186a229bf143b67f38
Eden Nahum <edennahu@post.bgu.ac.il>
2025-12-14 21:08:57 +0200

    fix

--- backend/src/modules/services/services.module.ts
+++ backend/src/modules/services/services.module.ts
@@ -1,14 +1,15 @@
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServiceEntity } from '../../entities/service.entity';
 import { ServicesController } from './services.controller';
 import { AdminServicesController } from './services.admin.controller';
-import {Module} from "@nestjs/common";
+import { ServicesService } from './services.service';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
     imports: [TypeOrmModule.forFeature([ServiceEntity]), AuthModule],
     controllers: [ServicesController, AdminServicesController],
-    providers: [],
-    exports: [],
+    providers: [ServicesService],
+    exports: [ServicesService],
 })
 export class ServicesModule {}

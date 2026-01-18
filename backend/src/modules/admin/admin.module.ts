17b1f0160f613b9f87c0266af210117b3c347ffc
eden2304 <55375496+eden2304@users.noreply.github.com>
2026-01-18 21:36:39 +0200

    Add admin business hours update endpoint

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { GalleryVideo } from '../../entities/gallery-video.entity';
 import { BackgroundVideo } from '../../entities/background-video.entity';
 import { BlockedTime } from '../../entities/blocked-time.entity';
 import { Setting } from '../../entities/setting.entity';
+import { BusinessHour } from '../../entities/business-hour.entity';
 import { AdminContentController } from './admin-content.controller';
 import { BlockedTimesController } from './blocked-times.controller';
 import { SettingsController } from './settings.controller';
@@ -13,16 +14,16 @@ import { AuthModule } from '../auth/auth.module';
 import {AdminController} from "./admin.controller";
 import { Appointment } from '../../entities/appointment.entity';
 import { AdminAppointmentsController } from './admin-appointments.controller';
+import { AdminBusinessHoursController } from './admin-business-hours.controller';
 
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Appointment, Product, Testimonial, GalleryVideo, BackgroundVideo, BlockedTime, Setting]),
+        TypeOrmModule.forFeature([Appointment, Product, Testimonial, GalleryVideo, BackgroundVideo, BlockedTime, Setting, BusinessHour]),
         AuthModule, // מספיק! כי AuthModule מייצא JwtModule
     ],
-    controllers: [AdminAppointmentsController, AdminContentController, AdminController, BlockedTimesController, SettingsController],
+    controllers: [AdminAppointmentsController, AdminContentController, AdminController, BlockedTimesController, SettingsController, AdminBusinessHoursController],
 })
 export class AdminModule {}
-
 
 
